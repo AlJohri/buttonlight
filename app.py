cdeedc97e93161d2331a3cb801fb06880738ae4d
Al Johri <al.johri@gmail.com>
2015-02-28 12:48:51 -0600

    change port

--- app.py
+++ app.py
@@ -62,4 +62,4 @@ api.add_resource(StatusList, '/status/')
 api.add_resource(Status, '/status/<ObjectId:status_id>')
 
 if __name__ == '__main__':
-	app.run(debug=True, host="0.0.0.0")
+	app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
